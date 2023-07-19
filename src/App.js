8b56f6845f326bc6e59ef2c29cb2c8811253903b
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-07-19 22:02:50 +0300

    add tests for sorting algorithms

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import "./App.css";
-import { quickSort } from "./logic/sortLogic";
-
-const sorted = quickSort([3, 2, 8, 4, 1, 6, 0, 3, 7, 9]);
 
 function App() {
-  return <div className="App">{sorted}</div>;
+  return <div className="App">Kalooli</div>;
 }
 
 export default App;
