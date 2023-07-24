0a4a4cc7d74622d55f833a2880a2376481a35364
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-07-24 22:47:27 +0300

    change bars to center + round bars

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,7 +5,7 @@ const Content = (props) => {
     <main className="w-full p-4 bg-[#463F3A] flex justify-center flex-auto flex-shrink-0">
       <div
         ref={barsRef}
-        className="max-w-7xl w-full grid grid-cols-[repeat(auto-fit,_1rem)] gap-2 justify-center transition-all duration-300"
+        className="max-w-7xl w-full grid grid-cols-[repeat(auto-fit,_1rem)] gap-2 justify-center items-center transition-all duration-300"
       >
         {arr.map((val, index) => {
           return (
