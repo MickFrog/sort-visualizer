61048075e6242938d34e5406e68e608c02886617
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-07-27 11:53:33 +0300

    intialise array on mount

--- src/App.js
+++ src/App.js
@@ -12,15 +12,16 @@ import {
   changeSpeed,
 } from "./logic/sortLogic";
 
-const myArr = [
-  87, 66, 42, 95, 56, 57, 74, 47, 62, 55, 85, 73, 77, 45, 39, 80, 69, 60, 43,
-  97, 79, 93, 68, 46, 52, 88, 71, 53, 44, 81,
-];
-
 function App() {
-  const [currArr, setCurrArr] = useState(myArr);
+  const [currArr, setCurrArr] = useState([]);
   const [arrayLength, setArrayLength] = useState(30);
   const barsArrayRef = useRef(null);
+
+  useEffect(() => {
+    //create array on mount
+    randomizeArray();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   useEffect(() => {
     //load up bar divs every time currArr changes.
